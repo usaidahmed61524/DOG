37c3c0c361af64a08c5b1270d0f2cd78a5375f4f
Bilal Husssain <bilal.mehfooz@soc-solution.com>
2023-09-18 22:14:33 -0700

    change aos config

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,27 +5,30 @@ import { IoLogoGameControllerB } from "react-icons/io";
 import { BsTwitter } from "react-icons/bs";
 import { AiFillInstagram } from "react-icons/ai";
 import logo from "../images/logo.png";
-
 const MyNavbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <div className="navbar-brand">
+        <div
+          className="navbar-brand"
+          data-aos="zoom-in"
+          data-aos-duration="3000"
+        >
           <Image src={logo} alt="logo" className="logo" />
         </div>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="myList">
             <li className="nav-item">
-              {/* <IoLogoGameControllerB className="navIcons" /> */}
+              <IoLogoGameControllerB className="navIcons" />
             </li>
 
             <li className="nav-item">
-              {/* <BsTwitter className="navIcons" /> */}
+              <BsTwitter className="navIcons" />
             </li>
 
             <li className="nav-item">
-              {/* <AiFillInstagram className="navIcons" /> */}
+              <AiFillInstagram className="navIcons" />
             </li>
           </ul>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Main from "./screens/main";
-import "aos/dist/aos.css";
 export default function Home() {
   return (
     <>
